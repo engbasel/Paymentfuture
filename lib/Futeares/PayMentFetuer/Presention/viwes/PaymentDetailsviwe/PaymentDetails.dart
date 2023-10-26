5f158f0ad11631766e9c5c77de8d1d9244de5ff2
basel embaby <engbasel2025@gmail.com>
2023-10-26 14:42:15 +0300

    Custom Credit Card

--- lib/Futeares/PayMentFetuer/Presention/viwes/PaymentDetailsviwe/PaymentDetails.dart
+++ lib/Futeares/PayMentFetuer/Presention/viwes/PaymentDetailsviwe/PaymentDetails.dart
@@ -1,13 +1,20 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:payapp/Futeares/PayMentFetuer/Presention/viwes/PaymentDetailsviwe/PayMentMethoListViwe.dart';
+import 'package:payapp/Futeares/PayMentFetuer/Presention/viwes/PaymentDetailsviwe/widgets/CoustomCredtCard.dart';
 import 'package:svg_flutter/svg.dart';
 
 import 'package:payapp/core/utls/style.dart';
 
-class PaymentDetailsViwe extends StatelessWidget {
+class PaymentDetailsViwe extends StatefulWidget {
   const PaymentDetailsViwe({super.key});
 
+  @override
+  State<PaymentDetailsViwe> createState() => _PaymentDetailsViweState();
+}
+
+class _PaymentDetailsViweState extends State<PaymentDetailsViwe> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,11 +38,14 @@ class PaymentDetailsViwe extends StatelessWidget {
           ),
         ),
       ),
-      body: const Column(
-        children: [
-          SizedBox(height: 20),
-          PaymentMethodsListView(),
-        ],
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            PaymentMethodsListView(),
+            CustomCreditCard()
+          ],
+        ),
       ),
     );
   }
---
+++ lib/Futeares/PayMentFetuer/Presention/viwes/PaymentDetailsviwe/widgets/CoustomCredtCard.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '';
+
+  String expiryDate = '';
+
+  String cardHolderName = '';
+
+  String cvvCode = '';
+
+  bool showBackView = false;
+
+  GlobalKey<FormState> formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          isHolderNameVisible: true,
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (value) {},
+        ),
+        CreditCardForm(
+            // isHolderNameVisible: true,
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            onCreditCardModelChange: (CreditCardModel) {
+              cardHolderName = CreditCardModel.cardHolderName;
+              cardNumber = CreditCardModel.cardNumber;
+              expiryDate = CreditCardModel.expiryDate;
+              cvvCode = CreditCardModel.cvvCode;
+              showBackView = CreditCardModel.isCvvFocused;
+              setState(() {});
+            },
+            formKey: formKey)
+      ],
+    );
+  }
+}
